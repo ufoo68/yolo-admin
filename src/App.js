b0cab02bb48c00e0fee34b79980617ab092111ec
ufoo68 <mitarou.yuutarou@gmail.com>
2019-10-27 15:29:30 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
+import axios from 'axios'
+import NumericInput from 'react-numeric-input'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: '---',
+      point: 0
+    }
+  }
+  
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+
+
+  render() {
+    const post = () => {
+      axios.put(`https://maidoary.herokuapp.com/customers/${this.state.result}`, {
+        point: this.state.point
+      })
+      .then(() => {
+        alert(`submit : ${this.state.point}`)
+      })
+      .catch((e) => {
+        alert(e)
+      })
+    }
+    return (
+      <div class="App">
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '100%' }}
+        />
+        <div class="id">User ID : {this.state.result}</div>
+        <div class="input">
+          <NumericInput value={this.state.point} min={-2} max={2}
+          onChange={(e) => {
+            this.setState({point: e.valueOf()})
+          }} />
+        </div>
+        {this.state.result === '---' ? 
+          <button disabled class="submitButton">QRコードをスキャンしてください</button>
+        :
+          <button onClick={post} class="submitButton">ポイントを加算</button>
+        }
+      </div>
+    )
+  }
 }
-
 export default App;

0766bb157b0502f6de1629163fe9b89c3c336ed9
ufoo68 <mitarou.yuutarou@gmail.com>
2019-10-27 10:19:41 +0900

    Updates

---
+++ precache-manifest.b147c7f0e7bba3c5adb9fed36b60f404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894e3b3f751a7ca865a922f4d417cfe4",
+    "url": "/yolo-admin/index.html"
+  },
+  {
+    "revision": "04ab333bbfb7b0eda4d1",
+    "url": "/yolo-admin/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1ee2b1c778cb84a9fbac",
+    "url": "/yolo-admin/static/js/2.c699e5e2.chunk.js"
+  },
+  {
+    "revision": "04ab333bbfb7b0eda4d1",
+    "url": "/yolo-admin/static/js/main.8f0ad2c9.chunk.js"
+  },
+  {
+    "revision": "93ab8af9565a000ce288",
+    "url": "/yolo-admin/static/js/runtime-main.eb97b420.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/yolo-admin/static/media/logo.25bf045c.svg"
+  }
+]);
